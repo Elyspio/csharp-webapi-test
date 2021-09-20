0da666b131c324aa3d2cf8deb496d910c320225d
Jonathan Guichard <jona.guich69@gmail.com>
2021-09-20 22:03:33 +0200

    initial

---
+++ Filters/Logs.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CSharpWebApiTest
+{
+    public class Logs : ActionFilterAttribute
+    {
+
+        public Logs()
+        {
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+
+            var controller = (ControllerBase)context.Controller;
+
+            var action = controller.ControllerContext.ActionDescriptor;
+
+
+            var arguments = context.ModelState.Aggregate("", (current, val) =>
+            {
+                var str = current;
+                if (str.Length != 0) str += ", ";
+
+                return $"{str} {val.Key}={val.Value?.AttemptedValue}";
+            });
+
+
+            Console.WriteLine(
+                "Exécution de l'action : {0}.{1} | idTechPsConnecte : {2} | arguments => {3}", action.ControllerName, action.ActionName, "Slavy", arguments
+            );
+
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext context)
+        {
+            var controller = (ControllerBase)context.Controller;
+
+            var action = controller.ControllerContext.ActionDescriptor;
+
+
+            var arguments = context.ModelState.Aggregate("", (current, val) =>
+            {
+                var str = current;
+                if (str.Length != 0) str += ", ";
+
+                return $"{str} {val.Key}={val.Value?.AttemptedValue}";
+            });
+
+            Console.WriteLine("Fin de l'action : {0}.{1} | idTechPsConnecte : {2} | arguments => {3}",
+                action.ControllerName,
+                action.ActionName,
+                "Slavy",
+                arguments
+            );
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+using CSharpWebApiTest;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add(typeof(Logs));
+});
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "CSharpWebApiTest", Version = "v1" });
+});
+
+builder.Configuration.AddJsonFile("Properties/appsettings.json", false);
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CSharpWebApiTest v1"));
+}
+
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
